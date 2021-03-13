74ea3a26d9c2d85783bc0d7387eed712e35cfda1
altafshaikh <iamaltafshaikh07@gmail.com>
2021-03-13 16:49:44 +0530

    🔥 Rename: endpoints.js

--- constants/endpoints.js
+++
@@ -1,3 +0,0 @@
-const BASE_URL = "https://ialtafshaikh.github.io/static-files/json/";
-
-export const contentstackDataURI = BASE_URL + "contentstack.json";
---
+++ constants/jsonEndpoint.js
@@ -0,0 +1,4 @@
+const BASE_URL =
+  "https://altafshaikh.github.io/static-file-storage/Portfolio/Json%20Data/";
+
+export const homeData = BASE_URL + "home.json";
