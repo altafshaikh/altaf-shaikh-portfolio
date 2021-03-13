6584a62063d1ed4c0b3a8f8611a228a267e8078b
altafshaikh <iamaltafshaikh07@gmail.com>
2021-03-13 14:24:02 +0530

    🔧 Config: NextJs config file added for images

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = (phase, { defaultConfig }) => {
+  return {
+    images: {
+      domains: [
+        "images.contentstack.io",
+        "www.contentstack.com",
+        "www.raweng.com",
+        "www.surfboardventures.com",
+      ],
+    },
+  };
+};
