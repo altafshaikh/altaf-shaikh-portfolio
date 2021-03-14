e8cea1fa4be51153b5829b808e6690532c51ed51
altafshaikh <iamaltafshaikh07@gmail.com>
2021-03-14 13:51:09 +0530

    ✨ DarkTheme toggling working

--- components/theme.js
+++ components/theme.js
@@ -1,13 +1,13 @@
 export const lightTheme = {
-  body: "#FFFFF",
-  text: "#00000",
+  body: "#E2E2E2",
+  text: "#363537",
   toggleBorder: "#FFF",
   gradient: "linear-gradient(#39598A, #79D7ED)",
 };
 
 export const darkTheme = {
-  body: "#OOOOOO",
-  text: "#FFFFFF",
+  body: "#363537",
+  text: "#FAFAFA",
   toggleBorder: "#6B8096",
   gradient: "linear-gradient(#091236, #1E215D)",
 };
