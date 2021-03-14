89f4b20c5358082135f93f6d67750ce136272481
altafshaikh <iamaltafshaikh07@gmail.com>
2021-03-14 13:39:50 +0530

    ✨ Theme js added

---
+++ components/theme.js
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  body: "#FFFFF",
+  text: "#00000",
+  toggleBorder: "#FFF",
+  gradient: "linear-gradient(#39598A, #79D7ED)",
+};
+
+export const darkTheme = {
+  body: "#OOOOOO",
+  text: "#FFFFFF",
+  toggleBorder: "#6B8096",
+  gradient: "linear-gradient(#091236, #1E215D)",
+};
