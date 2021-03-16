cd73b98b2f892441a6d299d82cadb875837a0fe4
altafshaikh <iamaltafshaikh07@gmail.com>
2021-03-16 12:31:52 +0530

    💄 Theme Toggler component implemented

--- components/themeToggler/index.js
+++ components/themeToggler/index.js
@@ -4,5 +4,16 @@ import React, { useContext } from "react";
 import { themeContext } from "../../pages/_app";
 
 export default function ThemeToggler() {
-  return <></>;
+  return (
+    <>
+      <label id="switch" className={togglerStyles["switch"]}>
+        <input type="checkbox" id="slider" />
+        <span
+          className={[togglerStyles["slider"], togglerStyles["round"]].join(
+            " "
+          )}
+        ></span>
+      </label>
+    </>
+  );
 }
