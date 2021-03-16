eadcf85d6c86377dd292617690ea6db952e97030
altafshaikh <iamaltafshaikh07@gmail.com>
2021-03-16 12:26:06 +0530

    🎨 Theme Toggler component added

---
+++ components/themeToggler/index.js
@@ -0,0 +1,8 @@
+import togglerStyles from "./index.module.scss";
+
+import React, { useContext } from "react";
+import { themeContext } from "../../pages/_app";
+
+export default function ThemeToggler() {
+  return <></>;
+}
