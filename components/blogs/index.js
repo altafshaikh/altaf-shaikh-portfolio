a8ae8020789de337e5ae0b485dd904b2426438f8
altafshaikh <iamaltafshaikh07@gmail.com>
2021-03-17 14:56:41 +0530

    💄 Blog component added

---
+++ components/blogs/index.js
@@ -0,0 +1,10 @@
+import blogStyles from "./index.module.scss";
+
+import React, { useContext } from "react";
+import { themeContext } from "../../pages/_app";
+
+export default function BlogSection() {
+  const { toggleDarkMode, theme } = useContext(themeContext);
+
+  return <></>;
+}
