b004e17e6b202051e554353cd361fb33d5003df1
altafshaikh <iamaltafshaikh07@gmail.com>
2021-03-29 15:00:53 +0530

    💄 Talk component : speaker image and speaker text implemented

--- components/talks/index.js
+++ components/talks/index.js
@@ -16,18 +16,20 @@ export default function TalkSection() {
         >
           <div className="row justify-content-end">
             <div className="col-md-7">
-              <h1 className={talkStyles["h1"]}>My Talks.</h1>
-              <p>
-                The first time I got asked to speak at a conference, my gut
-                reaction was “heck no. That sounds terrifying”.
-              </p>
+              <div className={[talkStyles["Speaking__text"]].join(" ")}>
+                <h1 className={talkStyles["h1"]}>My Talks.</h1>
+                <p className="mt-5">
+                  The first time I got asked to speak at a conference, my gut
+                  reaction was “heck no. That sounds terrifying”.
+                </p>
 
-              <p>
-                So naturally I said yes. Since then I’ve spoken at conferences
-                and meet-ups all over the world. It still terrifies me.
-              </p>
+                <p>
+                  So naturally I said yes. Since then I’ve spoken at conferences
+                  and meet-ups all over the world. It still terrifies me.
+                </p>
 
-              <p>Check out my videos and upcoming talks.</p>
+                <a href="#">Check out my videos and upcoming talks.</a>
+              </div>
             </div>
             <div className="col-md-5">
               <figure className={[talkStyles["Speaking__image"]].join(" ")}>
